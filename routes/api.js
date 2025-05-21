b6d368d6053e11fd51432e5a1902f71aabb10364
Joshua Wright <1981283+SuperJoshua@users.noreply.github.com>
2025-05-21 13:42:20 +0000

    fcc tests passed

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,115 @@
 "use strict"
 
-// a file that I edited
+// For those that come after me who may be reading this, if FCC still has these archived projects up, if you're inexplicably failing the FCC tests, check the order that you're checking for input. Mine fault was Test 9, where I was checking for a missing id before checking for missing update fields. To my mind, id checking should come first, but that wasn't the case for the tests.
+
+const e = require("cors")
+
+// a throwaway database
+const db = {"apitest": {"issues":[{"assigned_to":"d","status_text":"e","open":true,"_id":"1","issue_title":"a","issue_text":"b","created_by":"c","created_on":"2025-05-19T19:11:19.277Z","updated_on":"2025-05-19T19:11:19.277Z"},{"assigned_to":"i","status_text":"j","open":true,"_id":"1","issue_title":"f","issue_text":"g","created_by":"h","created_on":"2025-05-19T20:36:16.385Z","updated_on":"2025-05-19T20:36:16.385Z"}], index: 2}}
 
 module.exports = function(app) {
    app.route("/api/issues/:project")
    .get(function (req, res){
       const project = req.params.project
+
+      if (!db[project]) {res.send([])}
+
+      else {
+         /*
+         According to the live site example, I can apparently feed nonsense keys into the query, like "bubbles=1" and "potato=three", and those keys are simply ignored.
+
+         Entering a query that contains an acceptable key with an acceptable value, will filter the results accordingly -- possibly returning an empty array.
+
+         However, the query "open=maybe" (open expects a boolean), returns nothing -- not even an empty array. Was this intentional or simply an oversight?
+
+         Whatever the case, in my implementation, "bob=3" and "open=usually" both return [].
+         */
+         const entries = Object.entries(req.query)
+         if (entries.length > 0) {
+            const result = db[project].issues.filter(issue => {
+               let pass = true
+               for (const [key, value] of entries) {
+                  if (!(issue[key] === value)) {
+                     pass = false
+                     break
+                  }
+               }
+               if (pass) {return issue}
+            })
+
+            res.send(result)
+         }
+
+         else {res.send(db[project].issues)}
+      }
    })
    .post(function (req, res){
       const project = req.params.project
+      const body = req.body
+
+      if (body.created_by && body.issue_title && body.issue_text) {
+         if (!db[project]) {db[project] = {"issues": [], "index": 0}}
+
+         const assigned_to = (body.assigned_to) ? body.assigned_to : ""
+         const status_text = (body.status_text) ? body.status_text : ""
+         const open = true
+         const _id = String(db[project].index)
+         const issue_title = body.issue_title
+         const issue_text = body.issue_text
+         const created_by = body.created_by
+         const created_on = new Date().toISOString()
+         const updated_on = new Date().toISOString()
+
+         db[project].issues[_id] = {assigned_to, status_text, open, _id, issue_title, issue_text, created_by, created_on, updated_on}
+
+         db[project].index += 1
+
+         res.send(db[project].issues[_id])
+      }
+
+      else {res.send({"error": "required field(s) missing"})}
    })
    .put(function (req, res){
       const project = req.params.project
+      const body = req.body
+      const _id = body._id
+
+      if(!_id) {res.send({"error": "missing _id"})}
+
+      else if (!(body.assigned_to || body.status_text || body.open || body.issue_title || body.issue_text || body.created_by)) {res.send({"error": "no update field(s) sent", _id})}
+
+      else if (!db[project].issues[_id]) {res.send({"error": "could not update", _id})}
+
+      else {
+         if (!db[project].issues[_id].open) {res.send({"error": "could not update", _id})}
+
+         else {
+            db[project].issues[_id].assigned_to = (body.assigned_to) ? body.assigned_to : db[project].issues[_id].assigned_to
+            db[project].issues[_id].status_text = (body.status_text) ? body.status_text : db[project].issues[_id].status_text
+            // This checks if the "Check to close issue" checkbox is clicked.
+            db[project].issues[_id].open = (body.open) ? false : true
+            db[project].issues[_id].issue_title = (body.issue_title) ? body.issue_title : db[project].issues[_id].issue_title
+            db[project].issues[_id].issue_text = (body.issue_text) ? body.issue_text : db[project].issues[_id].issue_text
+            db[project].issues[_id].created_by = (body.created_by) ? body.created_by : db[project].issues[_id].created_by
+            db[project].issues[_id].updated_on = new Date().toISOString()
+
+            res.send({"result": "successfully updated", _id})
+         }
+      }
    })
    .delete(function (req, res){
       const project = req.params.project
+      const _id = req.body._id
+
+      if(!_id) {res.send({"error": "missing _id"})}
+
+      else {
+         if(!db[project].issues[_id]) {res.send({"error": "could not delete", _id})}
+
+         else {
+            db[project].issues[_id] = {"fate": "deleted"}
+            res.send({"result": "successfully deleted", _id})
+         }
+      }
    })
 }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,6 +2,8 @@
 
 // a file that I edited
 
+// For the most part, I feel like I'm writing these tests to match the output, in order to pass this project. This is the wrong way to go about something. This is exactly what certification (probably unintentionally) encourages. No wonder hiring managers ultimately don't care about certificates.
+
 const chaiHttp = require("chai-http")
 const chai = require("chai")
 const assert = chai.assert
@@ -12,113 +14,163 @@ chai.use(chaiHttp)
 suite("Functional Tests", function() {
    test("create an issue with every field", function(done) {
       chai.request(server)
-      .post("/api/issues/:project")
+      .post("/api/issues/apitest")
+      .send({"assigned_to":"d","status_text":"e","issue_title":"a","issue_text":"b","created_by":"c"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.assigned_to, "d")
+         assert.strictEqual(res.body.status_text, "e")
+         assert.strictEqual(res.body.issue_title, "a")
+         assert.strictEqual(res.body.issue_text, "b")
+         assert.strictEqual(res.body.created_by, "c")
          done()
       })
    })
    test("create an issue with only required fields", function(done) {
       chai.request(server)
-      .post("/api/issues/:project")
+      .post("/api/issues/apitest")
+      .send({"issue_title":"a","issue_text":"b","created_by":"c"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.issue_title, "a")
+         assert.strictEqual(res.body.issue_text, "b")
+         assert.strictEqual(res.body.created_by, "c")
          done()
       })
    })
    test("create an issue with missing required fields", function(done) {
       chai.request(server)
-      .post("/api/issues/:project")
+      .post("/api/issues/apitest")
+      .send({"issue_title":"a"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.error, "required field(s) missing")
          done()
       })
    })
    test("view issues on a project", function(done) {
       chai.request(server)
-      .get("/api/issues/:project")
+      .get("/api/issues/apitest")
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.isArray(res.body)
          done()
       })
    })
    test("view issues on a project with one filter", function(done) {
       chai.request(server)
-      .get("/api/issues/:project")
+      .get("/api/issues/apitest?open=true")
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.isArray(res.body)
+         if (res.body.length > 0) {
+            for (const item in res.body) {
+               assert.strictEqual(item.open, true)
+            }
+         }
          done()
       })
    })
    test("view issues on a project with multiple filters", function(done) {
       chai.request(server)
-      .get("/api/issues/:project")
+      .get("/api/issues/apitest?open=true&created_by=bob")
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.isArray(res.body)
+         if (res.body.length > 0) {
+            for (const item in res.body) {
+               assert.strictEqual(item.open, true)
+               assert.strictEqual(item.created_by, "bob")
+            }
+         }
          done()
       })
    })
    test("update one field on an issue", function(done) {
       chai.request(server)
-      .put("/api/issues/:project")
+      .put("/api/issues/apitest")
+      .send({"_id": "0", "status_text": "thinking about it"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.result, "successfully updated")
+         assert.strictEqual(res.body._id, "0")
          done()
       })
    })
    test("update multiple fields on an issue", function(done) {
       chai.request(server)
-      .put("/api/issues/:project")
+      .put("/api/issues/apitest")
+      .send({"_id": "0", "open": true, "status_text": "fixed it"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.result, "successfully updated")
+         assert.strictEqual(res.body._id, "0")
          done()
       })
    })
    test("update an issue with missing _id", function(done) {
       chai.request(server)
-      .put("/api/issues/:project")
+      .put("/api/issues/apitest")
+      .send({"created_by": "bob"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.error, "missing _id")
          done()
       })
    })
    test("update an issue with no fields to update", function(done) {
       chai.request(server)
-      .put("/api/issues/:project")
+      .put("/api/issues/apitest")
+      .send({ _id: '5f665eb46e296f6b9b6a504d' })
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         //assert.strictEqual(res.body.error, "no update field(s) sent")
+         //assert.strictEqual(res.body._id, "0")
+         assert.deepEqual(res.body, { error: 'no update field(s) sent', _id: '5f665eb46e296f6b9b6a504d' })
          done()
       })
    })
    test("update an issue with an invalid _id", function(done) {
       chai.request(server)
-      .put("/api/issues/:project")
+      .put("/api/issues/apitest")
+      .send({ _id: '5f665eb46e296f6b9b6a504d', issue_text: 'New Issue Text' })
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         //assert.strictEqual(res.body.error, "could not update")
+         //assert.strictEqual(res.body._id, "x")
+         assert.deepEqual(res.body, { error: 'could not update', _id: '5f665eb46e296f6b9b6a504d' })
          done()
       })
    })
    test("delete an issue", function(done) {
       chai.request(server)
-      .delete("/api/issues/:project")
+      .delete("/api/issues/apitest")
+      .send({"_id": "0"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.result, "successfully deleted")
+         assert.strictEqual(res.body._id, "0")
          done()
       })
    })
    test("delete an issue with an invalid _id", function(done) {
       chai.request(server)
-      .delete("/api/issues/:project")
+      .delete("/api/issues/apitest")
+      .send({"_id": "x"})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.error, "could not delete")
+         assert.strictEqual(res.body._id, "x")
          done()
       })
    })
    test("delete an issue with missing _id", function(done) {
       chai.request(server)
-      .delete("/api/issues/:project")
+      .delete("/api/issues/apitest")
+      .send({})
       .end(function(err, res) {
-         assert.strictEqual(res.body.string, "xxx")
+         assert.equal(res.status, 200)
+         assert.strictEqual(res.body.error, "missing _id")
          done()
       })
    })
